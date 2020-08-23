181d31126e5d6af0dea8737959003fe252b03bb3
Christiano Gontijo <christianogontijo@gmail.com>
2020-08-23 21:58:17 +1200

    Initial Commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.use('/api/v1/bootcamps', require('./bootcamps'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
